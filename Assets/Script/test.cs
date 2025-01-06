12172d6d010ce85d15be613cf9dedde7e3edb58b
rcorenti <corentin.reyleroux@ynov.com>
2025-01-06 10:18:58 +0100

    commit to test

---
+++ Assets/Script/test.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //rbzhfbez
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
