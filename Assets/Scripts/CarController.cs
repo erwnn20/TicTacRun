619d0afaca6b0a55036acdb1ee5e66179c3690cb
verwann <erwann.varlet@ynov.com>
2025-01-10 21:09:42 +0100

    Fixed acceleration problem when facing negative Z

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,11 +1,9 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CarInputManager))]
 public class CarController : MonoBehaviour
 {
-    [SerializeField] private CarInputManager input;
     [SerializeField] private List<WheelCollider> throttleWheels;
     [SerializeField] private List<WheelCollider> brakeWheels;
     [SerializeField] private List<WheelCollider> steeringWheels;
@@ -19,27 +17,27 @@ public class CarController : MonoBehaviour
     [SerializeField] private float maxTrunAxis = 20f;
 
     private Rigidbody _rb;
+    private CarInputManager _input;
 
     private void Start()
     {
-        input = GetComponent<CarInputManager>();
         _rb = GetComponent<Rigidbody>();
+        _input = GetComponent<CarInputManager>();
     }
 
     private void FixedUpdate()
     {
-        // acc se bloque en tournant
-        switch (input.throttle)
+        switch (_input.throttle)
         {
             case > 0 when IsMovingForward() || !IsMoving():
             case < 0 when IsMovingBackward() || !IsMoving():
-                Throttle(input.throttle * maxMotorTorque);
+                Throttle(_input.throttle * maxMotorTorque);
                 Brake(0);
                 break;
             case > 0:
             case < 0:
                 Throttle(0);
-                Brake(Mathf.Abs(input.throttle) * maxBrakePower);
+                Brake(Mathf.Abs(_input.throttle) * maxBrakePower);
                 break;
             case 0 when IsMoving():
                 Deceleration();
@@ -50,24 +48,9 @@ public class CarController : MonoBehaviour
                 break;
         }
 
+        if (_input.handBrake) Brake(maxBrakePower);
 
-        // pas de problems en tournant
-        /*throttleWheels.ForEach(
-            wheel => wheel.motorTorque = input.throttle > 0
-                ? input.throttle * maxMotorTorque
-                : 0
-        );
-        brakeWheels.ForEach(
-            wheel => wheel.brakeTorque = input.throttle < 0
-                ? Mathf.Abs(input.throttle) * maxBrakeTorque
-                : input.throttle == 0 && IsMoving()
-                    ? Deceleration()
-                    : 0
-        );*/
-
-        if (input.handBrake) Brake(maxBrakePower);
-
-        steeringWheels.ForEach(wheel => wheel.steerAngle = maxTrunAxis * input.steer);
+        steeringWheels.ForEach(wheel => wheel.steerAngle = maxTrunAxis * _input.steer);
     }
 
     private void Throttle(float motorTorque) => throttleWheels.ForEach(wheel => wheel.motorTorque = motorTorque);
@@ -76,7 +59,7 @@ public class CarController : MonoBehaviour
     private void Deceleration()
     {
         var speed = _rb.linearVelocity.magnitude;
-        var speedDirection = Math.Sign(_rb.linearVelocity.z);
+        var speedDirection = IsMovingForward() ? 1 : IsMovingBackward() ? -1 : 0;
 
         var deceleration = -speedDirection * DecelerationCalculation(speed);
         Throttle(deceleration);
@@ -86,7 +69,7 @@ public class CarController : MonoBehaviour
     private float DecelerationCalculation(float speed) =>
         maxDecelerationTorque * Mathf.Clamp01(-0.9f + Mathf.Exp(speed / speedThreshold));
 
-    private bool IsMovingForward() => _rb.linearVelocity.z > 0.15f;
-    private bool IsMovingBackward() => _rb.linearVelocity.z < -0.15f;
+    private bool IsMovingForward() => _rb.transform.InverseTransformDirection(_rb.linearVelocity).z > 0.1f;
+    private bool IsMovingBackward() => _rb.transform.InverseTransformDirection(_rb.linearVelocity).z < -0.1f;
     private bool IsMoving() => IsMovingForward() || IsMovingBackward();
 }
