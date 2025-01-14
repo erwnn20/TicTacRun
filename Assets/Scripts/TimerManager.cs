aa54d035246a84443d19217a7f4c45b3dca360c3
MelfStudent <melvin.favaro@ynov.com>
2025-01-14 16:12:29 +0100

    feat: added a timer system, with a sound that plays when the timer is less than 4 seconds

---
+++ Assets/Scripts/TimerManager.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TimerManager : MonoBehaviour
+{
+    public float timeRemaining = 10f;
+    public TextMeshProUGUI timerText;
+
+    public AudioSource tickSound;
+    private bool isGameOver = false;
+
+    private float previousTime;
+
+    void Start()
+    {
+        previousTime = Mathf.Floor(timeRemaining);
+    }
+
+    void Update()
+    {
+        if (!isGameOver)
+        {
+            timeRemaining -= Time.deltaTime;
+
+            if (timeRemaining <= 0)
+            {
+                timeRemaining = 0;
+                StopTickSound();
+                GameOver();
+            }
+
+            if (Mathf.Floor(timeRemaining) < previousTime)
+            {
+                HandleTickSound();
+                previousTime = Mathf.Floor(timeRemaining);
+            }
+
+            UpdateTimerDisplay();
+        }
+    }
+
+    public void AddTime(float timeToAdd)
+    {
+        timeRemaining += timeToAdd;
+        previousTime = Mathf.Floor(timeRemaining);
+    }
+
+    private void UpdateTimerDisplay()
+    {
+        if (timerText != null)
+        {
+            
+            if (timeRemaining > 10)
+            {
+                timerText.text = $"{timeRemaining:F0}";
+            }
+            else
+            {
+                timerText.text = $"{timeRemaining:F1}";
+            }
+        }
+    }
+
+    private void GameOver()
+    {
+        isGameOver = true;
+        Debug.Log("Game Over!");
+    }
+
+    private void HandleTickSound()
+    {
+        if (tickSound != null)
+        {
+            if (timeRemaining <= 4 && !tickSound.isPlaying)
+            {
+                float dynamicPitch = 1 + (4 - timeRemaining) * 0.25f;
+                tickSound.pitch = dynamicPitch;
+
+                tickSound.Play();
+            }
+            else if (timeRemaining > 4 && tickSound.isPlaying)
+            {
+                StopTickSound();
+            }
+        }
+    }
+
+    private void StopTickSound()
+    {
+        if (tickSound != null && tickSound.isPlaying)
+        {
+            tickSound.Stop();
+        }
+    }
+}
