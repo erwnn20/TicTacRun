c5fabba2b1f667291d61fc2332fbe90fe6c42e2e
MelfStudent <melvin.favaro@ynov.com>
2025-01-26 21:06:30 +0100

    feat: addition of a style and operational implementation of boosts and obstacles

--- Assets/Scripts/Boost/BoostManager.cs
+++ Assets/Scripts/Boost/BoostManager.cs
@@ -4,7 +4,6 @@ public class BoostManager : MonoBehaviour
 {
     public Transform[] boostPositions;
     public GameObject boostPrefab;
-    public float boostDuration = 10f;
 
     private GameObject currentBoost;
 
@@ -25,7 +24,6 @@ public class BoostManager : MonoBehaviour
             int randomIndex = Random.Range(0, boostPositions.Length);
             Vector3 randomPosition = boostPositions[randomIndex].position;
             currentBoost = Instantiate(boostPrefab, randomPosition, Quaternion.identity);
-            Destroy(currentBoost, boostDuration);
         }
     }
 
